cb3f19514c96f1868a17c8f2b35cd826b9d17aec
maria-namira <63401949+maria-namira@users.noreply.github.com>
2022-01-19 15:11:58 +0300

    w

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,5 @@
-import Button from './Button';
-import Tooltip from './Tooltip';
 import TooltipSwitch from './TooltipSwitch';
 
-const button = new Button('Click to toggle popover');
-const tooltip = new Tooltip(
-  'Tooltip title',
-  "And here's some amazing content. It's very engaging. Right?",
-);
-const tooltipSwitch = new TooltipSwitch(button, tooltip);
-tooltipSwitch.bindToDOM(document.querySelector('.component'));
-tooltipSwitch.init();
+const tooltipSwitch = new TooltipSwitch();
+
+tooltipSwitch.init(document.querySelector('.component'));
