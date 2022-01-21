5f3ae757ee17f4d005e267549f7a6615e3df987a
maria-namira <63401949+maria-namira@users.noreply.github.com>
2022-01-21 11:53:13 +0300

    s

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -23,7 +23,7 @@ describe('Credit Card Validator form', () => {
     });
     browser = await puppetteer.launch({
       headless: true,
-     */ slowMo: 100,
+      /* slowMo: 100,
       devtools: false, */
     });
     page = await browser.newPage();
